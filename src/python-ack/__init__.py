1ec56d74da81ae2eaf583b00bc3cd287b888fad3
Anton Sychev <anton@sychev.xyz>
2024-01-02 17:59:29 +0100

    feature: dev first push

---
+++ src/python-ack/__init__.py
@@ -0,0 +1,23 @@
+"""
+█▀ █▄█ █▀▀ █░█ █▀▀ █░█
+▄█ ░█░ █▄▄ █▀█ ██▄ ▀▄▀
+
+Author: <Anton Sychev> (anton at sychev dot xyz) 
+__init__.py (c) 2024 
+Created:  2024-01-02 17:37:05 
+Desc: Inilization of RocketStore
+"""
+
+__all__ = ["python-ack"]
+
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __build__,
+    __copyright__,
+    __description__,
+    __title__,
+    __url__,
+    __version__,
+    __license__,
+)
